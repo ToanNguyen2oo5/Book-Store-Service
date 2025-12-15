b565985f0a76de13a632665d4819923feb932faa
Hung Minh <minhlien2911@gmail.com>
2025-12-15 11:03:36 +0700

    nhóc tì sơn tây

--- src/main/java/com/bookstore/book_sell_service/enums/TrangThai.java
+++ src/main/java/com/bookstore/book_sell_service/enums/TrangThai.java
@@ -8,18 +8,17 @@ import java.util.Map;
 
 @Getter
 public enum TrangThai {
-    CHO_XAC_NHAN(1,"Chờ xác nhận"),
-    DA_XAC_NHAN(2,"Đã xác nhận"),
-    DANG_CHUAN_BI(3,"Đang chuẩn bị"),
-    DANG_GIAO(4,"Đang giao"),
-    DA_GIAO(5,"Đã giao"),
-    DA_HUY(6,"Đã hủy"),
-    TRA_HANG(7,"Trả hàng");
+    CHO_XAC_NHAN("Chờ xác nhận"),
+    DA_XAC_NHAN("Đã xác nhận"),
+    DANG_CHUAN_BI("Đang chuẩn bị"),
+    DANG_GIAO("Đang giao"),
+    DA_GIAO("Đã giao"),
+    DA_HUY("Đã hủy"),
+    TRA_HANG("Trả hàng");
 
     private final String moTa;
-    private final int value;
-    TrangThai(int value ,String moTa) {
-        this.value = value;
+
+    TrangThai(String moTa) {
         this.moTa = moTa;
     }
 
@@ -28,7 +27,7 @@ public enum TrangThai {
         return moTa;
     }
 
-    public int getValue() {return value;}
+
 
 
 }
